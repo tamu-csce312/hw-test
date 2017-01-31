f2fd8e82b84157cd012315420ce43136fcf3f67e
Vincent Zeng <yukun.zeng.hit@outlook.com>
2017-01-31 15:24:25 -0600

    added semi-tested solution for hex, dec and binary transformation

--- hw-1/solution/hex.cpp
+++ hw-1/solution/hex.cpp
@@ -4,10 +4,21 @@
 #include <functional>
 #include <string.h> // in case you want to use e.g. memset
 #include <math.h>
+#include <string>
+#include <bitset>
 
 using namespace std;
 
-long hexToDec(char *input, int strLen){
+// TODO how to deal with errors?
+string decToBinStr(long input){
+  int rem;
+  if(input / 2 != 0){
+    return (decToBinStr(input / 2) + to_string(input % 2));
+  }
+  return (to_string(input));
+}
+
+long hexStrToDec(char *input, int strLen){
   long sum = 0;
   //cout << "input length: " << strLen << endl;
   for(int i = strLen - 1; i >= 0; i --){
@@ -24,8 +35,27 @@ long hexToDec(char *input, int strLen){
   return sum;
 }
 
-long binToDec(char *input, int strLen){
+long binStrToDec(char *input, int strLen){
+  long sum = 0;
+  for(int i = strLen - 1; i >= 0; i --){
+    int digit = strLen - i - 1;
+    if(input[i] == '0');
+    else if(input[i] == '1')
+      sum += pow(2, digit);
+    else return 0;
+  }
+  return sum;
+}
 
+long strToDec(char *input, int strLen){
+  long sum = 0;
+  for(int i = strLen - 1; i >= 0; i --){
+    int digit = strLen - i - 1;
+    if(input[i] >= 48 && input[i] <= 57)
+      sum += (input[i] - 48) * pow(10, digit);
+    else return 0;
+  }
+  return sum;
 }
 
 int strLength(char* input){
@@ -38,8 +68,50 @@ int main(int argc, char *argv[]){
   if(argc != 4){
     cout << "Usage: " << "Usage: ./hex [ h | d | b ] [ h | d | b]" << endl;
   }
+  // input str length
   int length = strLength(argv[3]);
-  int dec = hexToDec(argv[3], length);
-  cout << "hexadecimal " << argv[3] << " is decimal " << dec << endl;
+  // input decimal as a long variable
+  long inputDec = 0;
+  // output str
+  string origin = "decimal", target = "decimal";
+  // translate input str to long type
+  switch (argv[1][0]){
+    case 'h':
+      inputDec = hexStrToDec(argv[3], length);
+      origin = "hexadecimal ";
+      break;
+    case 'd':
+      inputDec = strToDec(argv[3], length);
+      origin = "decimal ";
+      break;
+    case 'b':
+      inputDec = binStrToDec(argv[3], length);
+      origin = "binary ";
+      break;
+    default:
+      cout << "Usage: " << "Usage: ./hex [ h | d | b ] [ h | d | b]" << endl;
+  }
+  switch (argv[2][0]){
+    case 'h':
+      target = "hexadecimal ";
+      cout << origin << argv[3] << " is " << target
+        << hex << inputDec << endl;
+      break;
+    case 'd':
+      target = "decimal ";
+      cout << origin << argv[3] << " is " << target
+        << inputDec << endl;
+      break;
+    case 'b':
+      target = "binary ";
+      cout << origin << argv[3] << " is " << target
+        << decToBinStr(inputDec) << endl;
+      break;
+    default:
+      cout << "Usage: " << "Usage: ./hex [ h | d | b ] [ h | d | b]" << endl;
+  }
+  //cout << origin << argv[3] << " is decimal " << inputDec << endl;
+  //cout << hex << inputDec << endl;
+  //cout << decToBinStr(inputDec) << endl;
 }
 
